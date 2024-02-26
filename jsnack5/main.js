04226f559495d0970c1d0b3080f72066c60e8f2c
alemtt <ale@mattera.eu>
2024-02-26 12:01:46 +0100

    add while jsnack5

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -8,16 +8,32 @@ if(number % 2 == 0) {
 } else {
     dispari.push(number)
 }
-*/
 for (let i = 0; i < 6; i++) {
     const number = parseInt(prompt('inserisci qui un numero'))
+    
+    if (number % 2 == 0) {
+        console.log('number is even')
+        
+    } else {
+        dispari.push(number)
+        console.log(dispari)
+    }
+    
+}
+*/
+let i = 0;
 
-if(number % 2 == 0) {
-    console.log('number is even')
-    
-} else {
-    dispari.push(number)
-    console.log(dispari)
+while (i < 6) {
+    const number = parseInt(prompt('inserisci qui un numero'))
+
+    if (number % 2 == 0) {
+        console.log('number is even')
+
+    } else {
+        dispari.push(number)
+        console.log(dispari)
+    }
+
+    i++
 }
-    
-}
+
