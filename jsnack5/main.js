0a5c55210775dff98b33f98eb0184f6a7efdab1f
alemtt <ale@mattera.eu>
2024-02-26 11:58:10 +0100

    add for   jsnack5

---
+++ jsnack5/main.js
@@ -0,0 +1,23 @@
+const dispari = []
+/*
+const number = parseInt(prompt('inserisci qui un numero'))
+
+if(number % 2 == 0) {
+    console.log('number is even')
+    
+} else {
+    dispari.push(number)
+}
+*/
+for (let i = 0; i < 6; i++) {
+    const number = parseInt(prompt('inserisci qui un numero'))
+
+if(number % 2 == 0) {
+    console.log('number is even')
+    
+} else {
+    dispari.push(number)
+    console.log(dispari)
+}
+    
+}
