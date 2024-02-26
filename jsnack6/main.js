f542f81ab2312d703fee4793ceadc1e498db3500
alemtt <ale@mattera.eu>
2024-02-26 12:37:06 +0100

    add while jsnack6

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -2,10 +2,18 @@
 
 const cube = number * number * number
 console.log(cube);
-*/
 for (let i = 0; i < 5; i++) {
     const number = parseInt(prompt('inserisci qui un numero'))
     const cube = number * number * number
     console.log(cube);
 }
 
+*/42
+let i = 0
+while(i < 5){
+    const number = parseInt(prompt('inserisci qui un numero'))
+    const cube = number * number * number
+    console.log(cube);
+    
+    i++
+}
