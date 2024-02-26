eecb5d80e5e80116acca386ef15a324dcd35fc7e
alemtt <ale@mattera.eu>
2024-02-26 12:19:07 +0100

    add for jsnack6

---
+++ jsnack6/main.js
@@ -0,0 +1,11 @@
+/*const number = parseInt(prompt('inserisci qui un numero'))
+
+const cube = number * number * number
+console.log(cube);
+*/
+for (let i = 0; i < 5; i++) {
+    const number = parseInt(prompt('inserisci qui un numero'))
+    const cube = number * number * number
+    console.log(cube);
+}
+
